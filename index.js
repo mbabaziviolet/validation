37ebb6b7826d9ec66e5fd82fcdb4ff109293fd99
mbabaziviolet <mbabazijoy549@gmail.com>
2022-02-10 23:08:55 -0800

    assessment

---
+++ index.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('#survey-form');
+const usernameInput = document.querySelector("#username")
+const emailInput=document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const confirmpasswordInput =mdocument.querySelector("#confirm-password");
+
+
+const myform= document.querySelector("#container");
+myform.addEventListener('submit', formSubmitted);
+
+
